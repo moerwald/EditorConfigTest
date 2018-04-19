56e0b4dc1ab159323ab8457c7899f30c9b100fdc
Mewald Andreas <MewaldA@pdts.at>
2018-04-19 15:26:05 +0200

    Add project files.

---
+++ EditorConfig/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EditorConfig
+{
+    class positiveInteger
+    {
+        public positiveInteger(int value)
+        {
+            Value = value;
+        }
+
+        private void SomeMethod()
+        {
+            Value = 42;
+
+            int x = 42;
+
+        }
+
+        public int Value { get; set; }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
